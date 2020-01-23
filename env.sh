d0f6df868603be3193c6f91ea4759bd948deff54
Hamza Nanouche <hamza.nanouche@gmail.com>
2020-01-23 17:44:44 +0100

    added env file for complilng ada

---
+++ env.sh
@@ -0,0 +1,2 @@
+export PATH=$HOME/opt/GNAT/2019-arm-elf/bin:$HOME/opt/GNAT/2019/bin:$HOME/stlink/build/Release:$HOME/stlink/build/Release/src/gdbserver:$PATH
+export GPR_PROJECT_PATH=$HOME/opt/Ada_Drivers_Library:$GPR_PROJECT_PATH
